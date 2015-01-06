233300f45e6725850b2fa0e3a8eea23d2eabe0da
Milos Zivadinovic <mzdv@outlook.com>
2015-01-07 00:31:12 +0100

    Added full flashcard functionality.

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -4,19 +4,26 @@
 var express = require("express");
 var router = express.Router();
 
-var mock_data = require("./mockData");
+var _ = require("lodash");
+
+var mock_data = require("../mock_data");
 
 router
     .get('/', function(req, res) {
         res.render("flashcards", {
             flashcards: mock_data
-        })
+        });
     })
     .get('/:id', function(req, res) {
-
+        var flashcard = _.find(mock_data, {'id': req.params.id});
+        res.render("single_flashcard", {
+            flashcard: flashcard
+        });
     })
-    .get('/:id/solution', function(req, res) {
-
+    .get('/:id/answer', function(req, res) {
+        res.render("single_answer", {
+            answer: _.find(mock_data, {'id': req.params.id}).answer
+        });
     });
 
 module.exports = router;
